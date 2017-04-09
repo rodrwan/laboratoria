4c1b7782e18d152c09e84a73f77915be19d20500
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2017-04-09 20:03:22 -0300

    add more posible solution and measure it performance

--- ad_hoc/src/exercise_7/is_duplicate.js
+++ ad_hoc/src/exercise_7/is_duplicate.js
@@ -21,6 +21,41 @@
     return false
   }
 
+  function isDuplicate2 (arr) {
+    arr = arr.sort((a,b) => a-b)
+    for (let i = 0; i < arr.length-1; i++) {
+      if (arr[i] === arr[i+1]) return true
+    }
+
+    return false
+  }
+
+  function isDuplicate3 (arr) {
+    const numbers = {}
+    for (let i = 0; i < arr.length; i++) {
+      if (numbers.hasOwnProperty(arr[i])) {
+        return true
+      }
+      numbers[arr[i]] = true
+    }
+
+    return false
+  }
+
+  function isDuplicate4 (arr) {
+    for (let i = 0; i < arr.length; i++){
+      for (let j = 0; j < i; j++){
+        if (arr[j] == arr[i]) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
   exports.isDuplicate = isDuplicate
+  exports.isDuplicate2 = isDuplicate2
+  exports.isDuplicate3 = isDuplicate3
+  exports.isDuplicate4 = isDuplicate4
 
 })(typeof window === 'undefined' ? module.exports : window)
--- ad_hoc/test/exercise_7.spec.js
+++ ad_hoc/test/exercise_7.spec.js
@@ -1,12 +1,45 @@
 const expect = require('expect.js')
-const isDuplicate = require('../src/exercise_7/is_duplicate').isDuplicate
+const { isDuplicate, isDuplicate2, isDuplicate3, isDuplicate4 } = require('../src/exercise_7/is_duplicate')
 
 describe('Exercise 7: is duplicate.', () => {
-  it('should return true if repeated values.', () => {
-    const arr = [1,2,3,3,,4,5]
-    const expectedValue = true
-    const actualValue = isDuplicate(arr)
-
-    expect(actualValue).to.eql(expectedValue)
+  describe('Method 1', () => {
+    it('should return true if repeated values.', () => {
+      const arr = [1,2,3,3,,4,5]
+      const expectedValue = true
+      console.time('isDuplicate version 1')
+      const actualValue = isDuplicate(arr)
+      console.timeEnd('isDuplicate version 1')
+      expect(actualValue).to.eql(expectedValue)
+    })
+  })
+  describe('Method 2', () => {
+    it('should return true if repeated values.', () => {
+      const arr = [1,2,3,3,,4,5]
+      const expectedValue = true
+      console.time('isDuplicate version 2')
+      const actualValue = isDuplicate2(arr)
+      console.timeEnd('isDuplicate version 2')
+      expect(actualValue).to.eql(expectedValue)
+    })
+  })
+  describe('Method 3', () => {
+    it('should return true if repeated values.', () => {
+      const arr = [1,2,3,3,,4,5]
+      const expectedValue = true
+      console.time('isDuplicate version 3')
+      const actualValue = isDuplicate3(arr)
+      console.timeEnd('isDuplicate version 3')
+      expect(actualValue).to.eql(expectedValue)
+    })
+  })
+  describe('Method 4', () => {
+    it('should return true if repeated values.', () => {
+      const arr = [1,2,3,3,,4,5]
+      const expectedValue = true
+      console.time('isDuplicate version 4')
+      const actualValue = isDuplicate4(arr)
+      console.timeEnd('isDuplicate version 4')
+      expect(actualValue).to.eql(expectedValue)
+    })
   })
 })
