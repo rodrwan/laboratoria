b129abc1ded126830cb50bd7815a7aa8eaccd24b
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2017-04-09 21:14:42 -0300

    add more tast case.

--- ad_hoc/test/exercise_7.spec.js
+++ ad_hoc/test/exercise_7.spec.js
@@ -4,41 +4,79 @@ const { isDuplicate, isDuplicate2, isDuplicate3, isDuplicate4 } = require('../sr
 describe('Exercise 7: is duplicate.', () => {
   describe('Method 1', () => {
     it('should return true if repeated values.', () => {
-      const arr = [1,2,3,3,,4,5]
+      const arr = [1,2,3,3,4,4,5]
       const expectedValue = true
-      console.time('isDuplicate version 1')
+      console.time('\tisDuplicate true version 1')
       const actualValue = isDuplicate(arr)
-      console.timeEnd('isDuplicate version 1')
+      console.timeEnd('\tisDuplicate true version 1')
+      expect(actualValue).to.eql(expectedValue)
+    })
+    it('should return false if no value is repeated.', () => {
+      const arr = [1,2,3,4,5]
+      const expectedValue = false
+      console.time('\tisDuplicate false version 1')
+      const actualValue = isDuplicate(arr)
+      console.timeEnd('\tisDuplicate false version 1')
       expect(actualValue).to.eql(expectedValue)
     })
   })
+
   describe('Method 2', () => {
     it('should return true if repeated values.', () => {
-      const arr = [1,2,3,3,,4,5]
+      const arr = [1,2,3,3,6,4,5]
       const expectedValue = true
-      console.time('isDuplicate version 2')
+      console.time('\tisDuplicate true version 2')
       const actualValue = isDuplicate2(arr)
-      console.timeEnd('isDuplicate version 2')
+      console.timeEnd('\tisDuplicate true version 2')
+      expect(actualValue).to.eql(expectedValue)
+    })
+
+    it('should return false if no value is repeated.', () => {
+      const arr = [1,2,3,4,5]
+      const expectedValue = false
+      console.time('\tisDuplicate false version 2')
+      const actualValue = isDuplicate2(arr)
+      console.timeEnd('\tisDuplicate false version 2')
       expect(actualValue).to.eql(expectedValue)
     })
   })
+
   describe('Method 3', () => {
     it('should return true if repeated values.', () => {
-      const arr = [1,2,3,3,,4,5]
+      const arr = [1,2,3,3,4,5]
       const expectedValue = true
-      console.time('isDuplicate version 3')
+      console.time('\tisDuplicate true version 3')
       const actualValue = isDuplicate3(arr)
-      console.timeEnd('isDuplicate version 3')
+      console.timeEnd('\tisDuplicate true version 3')
+      expect(actualValue).to.eql(expectedValue)
+    })
+
+    it('should return false if no value is repeated.', () => {
+      const arr = [1,2,3,4,5]
+      const expectedValue = false
+      console.time('\tisDuplicate false version 3')
+      const actualValue = isDuplicate4(arr)
+      console.timeEnd('\tisDuplicate false version 3')
       expect(actualValue).to.eql(expectedValue)
     })
   })
+
   describe('Method 4', () => {
     it('should return true if repeated values.', () => {
-      const arr = [1,2,3,3,,4,5]
+      const arr = [1,2,3,3,4,5]
       const expectedValue = true
-      console.time('isDuplicate version 4')
+      console.time('\tisDuplicate true version 4')
       const actualValue = isDuplicate4(arr)
-      console.timeEnd('isDuplicate version 4')
+      console.timeEnd('\tisDuplicate true version 4')
+      expect(actualValue).to.eql(expectedValue)
+    })
+
+    it('should return false if no value is repeated.', () => {
+      const arr = [1,2,3,4,5]
+      const expectedValue = false
+      console.time('\tisDuplicate false version 4')
+      const actualValue = isDuplicate4(arr)
+      console.timeEnd('\tisDuplicate false version 4')
       expect(actualValue).to.eql(expectedValue)
     })
   })
